a34b471818a58daa83a6b14b52f697f831ad3ffe
kevindskim <kevindskim@naver.com>
2024-08-15 08:49:11 +0900

    texture-basic project is completed.

--- texture-basic/src/Texture.cpp
+++ texture-basic/src/Texture.cpp
@@ -5,7 +5,7 @@ Texture::Texture(const std::string & path)
 	: m_TextImagID {0}, m_FilePath {path}, m_LocalBuffer{ nullptr }, m_Width{ 0 }, m_Height{ 0 }, m_BPP{ 0 }
 {
 	stbi_set_flip_vertically_on_load(1); //텍스처 위아래를 뒤집음(OpenGL은 좌표계 다름)
-	m_LocalBuffer = stbi_load(path.c_str(), &m_Width, &m_Height, &m_BPP, 4); //디스크에서 이미지 읽어오기
+	m_LocalBuffer = stbi_load(path.c_str(), &m_Width, &m_Height, &m_BPP, 0); //디스크에서 이미지 읽어오기
 
 	GLCall(glGenTextures(1, &m_TextImagID)); //텍스처 버퍼 생성
 	GLCall(glBindTexture(GL_TEXTURE_2D, m_TextImagID)); //바인딩
@@ -35,7 +35,7 @@ Texture::~Texture()
 void Texture::Bind(unsigned int slot) const
 {
 	GLCall(glActiveTexture(GL_TEXTURE0 + slot)); //텍스처는 활성화 할 수 있는 슬롯이 한정되어 있으며, 이를 명시해야 함
-	GLCall(glBindTexture(GL_TEXTURE_2D, this->m_TextImagID));
+	GLCall(glBindTexture(GL_TEXTURE_2D, m_TextImagID));
 }
 
 void Texture::Unbind() const
